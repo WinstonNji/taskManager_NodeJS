fa2d23c6ed7c27e99cbdfc768e00e51a0a42e403
Winston Nji <winston01russel@gmail.com>
2025-07-11 17:20:35 +0200

    Created user model and jobCreationModel

---
+++ jobApi/Model/job.model.js
@@ -0,0 +1,29 @@
+const mongoose = require ('mongoose')
+const Schema = mongoose.Schema
+
+const JobSchema = new Schema ({
+    company : {
+        type : String,
+        require: [true, 'Enter a company name']
+    },
+
+    role : {
+        type: String
+    },
+
+    status: {
+        type: String,
+        enum : [
+            'interview', 'declined', 'pending'
+        ],
+        default: pending
+    },
+
+    createdBy: {
+        type: mongoose.Types.ObjectId, ref: 'User',
+        require: [true,  'Please provide User']
+    }
+}, {timestamps: true})
+
+module.exports = mongoose.model('job', JobSchema)
+
---
+++ jobApi/Model/userResitrationModel.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        required : true, 
+    },
+
+    email : {
+        type: String,
+        required: [true, 'enter a valid email'],
+        unique: true,
+        match : [
+            /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/, 'Please provide a valid email'
+        ]
+    },  
+
+    password : {
+        type: String,
+        minlength : 8
+
+    }
+}, {timestamps: true})
+
+module.exports = mongoose.model('User', UserSchema)
