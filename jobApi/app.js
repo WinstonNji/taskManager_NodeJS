50d807a3564ddeda0015633e7ea6f22aaf27b06f
Winston Nji <winston01russel@gmail.com>
2025-07-12 14:17:00 +0200

    implemented security middleware

--- jobApi/app.js
+++ jobApi/app.js
@@ -5,7 +5,26 @@ const authRoutes = require('./Router/auth.routes')
 const jobRoutes = require('./Router/job.routes')
 const mongoose = require('mongoose')
 
+// Security
+const helmet = require('helmet');
+const cors = require('cors');
+const xss = require('xss-clean');
+const rateLimit = require('express-rate-limit');
+const mongoSanitize = require('express-mongo-sanitize');
+const hpp = require('hpp');
+
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+app.use(mongoSanitize());
+app.use(hpp());
+
 app.use(express.json())
+
+app.use(rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100
+}));
 
 const userURI = process.env.USER_URI
 
